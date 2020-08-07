6711a018ed83455ccf6b17cd4c93e81c776a171a
johair84 <33111357+johair84@users.noreply.github.com>
2020-08-08 00:49:38 +0600

    arrow done

---
+++ template.js
@@ -0,0 +1,16 @@
+const firstName = "Justin";
+const lastName = "TimberLake";
+const fullName = firstName + " " + lastName + " is a boy!";
+console.log(fullName);
+const fullName2 = `${firstName} ${lastName} is a boy`;
+console.log(fullName2);
+
+
+const multiLine = "I am Johiar Mahtab\n"
+    + "I am from Chittagong\n"
+    + "I am form Bangladesh\n"
+
+const multiLine2 = `I am Johair Mahatab
+I am form Chittagong
+I am form Bangaldesh`;
+console.log(multiLine2);
